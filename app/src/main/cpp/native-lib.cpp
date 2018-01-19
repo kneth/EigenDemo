ee6cefec260fc9ecee47858f2a47912371f8fe0f
Kenneth Geisshirt <geisshirt@gmail.com>
2018-01-19 23:21:37 +0100

    Initial import of project

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,21 @@
+#include <jni.h>
+#include <string>
+
+#include "Eigen/Eigen/Dense"
+
+using Eigen::MatrixXd;
+
+extern "C" JNIEXPORT jint
+JNICALL
+Java_zigzak_net_eigendemo_MainActivity_computeDeterminant(
+        JNIEnv *env,
+        jobject /* this */) {
+
+    MatrixXd m(2, 2);
+    m(0, 0) = 3;
+    m(1, 0) = 2.5;
+    m(0, 1) = -1;
+    m(1, 1) = m(1, 0) + m(0, 1);
+
+    return m.determinant();
+}
---
+++ app/src/main/java/zigzak/net/eigendemo/MainActivity.kt
@@ -0,0 +1,31 @@
+package zigzak.net.eigendemo
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Example of a call to a native method
+        sample_text.text = computeDeterminant().toString()
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    private external fun computeDeterminant() : Int
+
+
+    companion object {
+
+        // Used to load the 'native-lib' library on application startup.
+        init {
+            System.loadLibrary("native-lib")
+        }
+    }
+}
